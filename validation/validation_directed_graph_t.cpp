5b1e25fa0f65974679d2b76b5e42ef5f6a29568c
Antoine Allard <antoine.allard.1@gmail.com>
2020-11-20 12:09:04 -0500

    updated copyright

--- validation/validation_directed_graph_t.cpp
+++ validation/validation_directed_graph_t.cpp
@@ -8,10 +8,10 @@
  *
  *  Author:  Antoine Allard
  *  WWW:     antoineallard.info
- *  Date:    August 2019
+ *  Date:    August 2020
  *
  *
- *  Copyright (C) 2019 Antoine Allard
+ *  Copyright (C) 2020 Antoine Allard
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
